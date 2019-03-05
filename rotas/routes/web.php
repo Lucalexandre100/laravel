70077549236bada6a7dbb58dbdcc174dc440279d
aplucas <l.alexandre100@gmail.com>
2019-03-05 09:10:59 -0300

    Exemplos de roterização

--- rotas/routes/web.php
+++ rotas/routes/web.php
@@ -42,10 +42,61 @@ Route::get('/repetir/{nome}/{n}', function ($nome, $n) {
 /**
  * Recebendo parametros da url
  * Retornando os parametros recebido da url
- * Restringindo tipos de parametros na url
+ * Restringindo tipos de parametros na url(regex)
  */
 Route::get('/restringir/{nome}/{n}', function ($nome, $n) {
     for ($i = 0; $i < $n; $i++) {
         echo "<h1> Nome: $nome </h1>";
     }
 })->where('n', '[0-9]+');
+
+/**
+ * Recebendo parametros da url
+ * Retornando os parametros recebido da url
+ * Parametros opcionais
+ */
+Route::get('/opcional/{nome?}', function ($nome = null) {
+    if (isset($nome)) {
+        echo "<h1> Nome: $nome </h1>";
+    } else {
+        echo "Sem parametro.";
+    }
+});
+
+/**
+ * Agrupando rotas
+ */
+Route::prefix('/app')->group(function () {
+    Route::get('/', function () {
+        return "Página principal";
+    });
+    Route::get('/profile', function () {
+        return "Página de perfil";
+    });
+    Route::get('/about', function () {
+        return "Página sobre ";
+    });
+});
+
+/**
+ * Redirecionamento de rotas
+ */
+Route::redirect('/', '/teste', 301);
+
+/** Retornando uma view
+ * o promeiro parâmetro é o nome da url
+ * o Segundo parâmetro é o nome da view. ex.: resources/views/welcome.blade.php
+ */
+Route::view('/hello', 'hello');
+
+/**
+ * Passando parâmetros para a view
+ */
+Route::view('/person', 'person', ['name' => 'Lucas', 'last_name' => 'Alexandre']);
+
+/**
+ * Recebendo parametros da url e passando para a view
+ */
+Route::get('/person/{name}/{last_name}', function ($name, $last_name) {
+    return view('person', ['name' => $name, 'last_name' => $last_name]);
+});
